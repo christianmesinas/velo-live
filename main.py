d5ed8d53eabfa4eab63e6103833a4afb3fb3345c
christian <guzmanchristian@hotmail.com>
2025-07-11 19:15:53 +0200

    fix live

--- main.py
+++ main.py
@@ -103,4 +103,5 @@ def inject_language():
 
 # Opstart app (alleen lokaal, Render gebruikt gunicorn)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(getenv('PORT', 8000)), debug=True)
+    port = int(getenv('PORT', 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
